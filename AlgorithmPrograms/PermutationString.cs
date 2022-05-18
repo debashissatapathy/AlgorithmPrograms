5057dcfd23f4cac9f18fdb14b329532ff6e5c5c9
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-05-18 13:18:28 +0530

    [Debashis] Insert Permutation String

---
+++ AlgorithmPrograms/PermutationString.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmPrograms
+{
+    class PermutationString
+    {
+        internal static void permute(string str, int startindex, int endindex)
+        {
+            if (startindex == endindex)
+            {
+                Console.WriteLine(str);
+            }
+            else
+            {
+                for (int i = startindex; i <= endindex; i++)
+                {
+                    
+                    str = swap(str, startindex, i);                    
+                    permute(str, startindex + 1, endindex);                    
+                    str = swap(str, startindex, i);
+                }
+            }
+        }
+        
+        internal static string swap(string a, int i, int j)
+        {
+            char temp;
+            char[] charArray = a.ToCharArray();
+            temp = charArray[i];
+            charArray[i] = charArray[j];
+            charArray[j] = temp;
+            string s = new string(charArray);            
+            return s;
+        }
+    }
+}
+
---
+++ AlgorithmPrograms/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace AlgorithmPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool status = true;
+            while (status)
+            {
+                Console.WriteLine("Algorithms Problems:\n1)Permutation of string\n2)Bubble Sort\n");
+                int op = Convert.ToInt32(Console.ReadLine());
+                switch (op)
+                {
+                    case 1:
+                        Console.WriteLine("********* Permutations of a String**********");
+                        Console.WriteLine("Enter String");
+                        var str = Console.ReadLine();
+                        int n = str.Length;
+                        PermutationString.permute(str, 0, n - 1);
+                        break;
+                    //case 2:
+                    //    Console.WriteLine("********* Bubble Sort**********");
+                    //    BubbleSort b = new BubbleSort();
+                    //    b.BSort();
+                    //    break;
+
+                }
+            }
+
+        }
+    }
+}
