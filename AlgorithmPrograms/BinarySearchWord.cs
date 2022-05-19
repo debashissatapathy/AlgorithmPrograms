76b0a98688797b3cc7d19241944a1150d7905b4a
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-05-19 10:57:04 +0530

    [Debashis] Insert BinarySearchWord

--- AlgorithmPrograms/BinarySearchWord.cs
+++ AlgorithmPrograms/BinarySearchWord.cs
@@ -6,5 +6,21 @@ namespace AlgorithmPrograms
 {
     class BinarySearchWord
     {
+        public int Binary_Search_Word(string[] arr, string key)
+        {
+            int left = 0, right = (arr.Length - 1);
+
+            while (left <= right)
+            {
+                int mid = Math.Abs((left + right) / 2);
+                if (arr[mid] == key)
+                    return mid;
+                else if (key.CompareTo(arr[mid]) > 0)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+            return -1;
+        }
     }
 }
--- AlgorithmPrograms/Program.cs
+++ AlgorithmPrograms/Program.cs
@@ -9,7 +9,7 @@ namespace AlgorithmPrograms
             bool status = true;
             while (status)
             {
-                Console.WriteLine("Algorithms Problems:\n1. Permutation of string\n2. Bubble Sort\n3. Insertion Sort");
+                Console.WriteLine("Algorithms Problems:\n1. Permutation of string\n2. Bubble Sort\n3. Insertion Sort\n4. Binary Search Word\n5. Merge Sort");
                 int op = Convert.ToInt32(Console.ReadLine());
                 switch (op)
                 {
@@ -30,7 +30,43 @@ namespace AlgorithmPrograms
                         InsertionSort insert = new InsertionSort();
                         insert.Insertion_Sort();
                         break;
+                    case 4:
 
+                        BinarySearchWord bs = new BinarySearchWord();
+                        string[] arr = { "apple", "ball", "cat", "dog", "egg", "fish" };
+                        foreach (string s in arr)
+                        {
+                            Console.WriteLine(s);
+                        }
+                        Array.Sort(arr);
+                        Console.WriteLine("Please enter a string to be searched :");
+                        string key = Console.ReadLine();
+                        int result = bs.Binary_Search_Word(arr, key);
+                        if (result == -1)
+                            Console.WriteLine("Element not present");
+                        else
+                            Console.WriteLine("Element found at "
+                                            + "index " + result);
+                        Console.WriteLine(result);
+                        break;
+                    case 5:
+                        MergeSort merge = new MergeSort();
+                        
+                        int[] array = { 49, 3, 53, 21, 27, 75, 50, 41 };
+                        Console.WriteLine("Merge Sort");
+                        Console.Write("Initial array is: ");
+                        for (int i = 0; i < 8; i++)
+                        {
+                            Console.Write(array[i] + " ");
+                        }                       
+                        merge.MergeSort1(array, 0,   7);
+                        Console.Write("\nSorted Array is: ");
+                        for (int i = 0; i < 8; i++)
+                        {
+                            Console.Write(array[i] + " ");
+                        }
+                        Console.WriteLine();
+                        break;
                 }
                 Console.Write("\n");
             }
